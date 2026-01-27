3476fcade17ff194585031600c7ba4f288f66f70
Devraj Gavit <dvrjgavit@gmail.com>
2026-01-28 01:35:07 +0530

    Changes for vercel

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Mail, Phone, MapPin, Facebook, Twitter, Instagram, Linkedin } from 'lucide-react';
+import { Mail, Phone, MapPin } from 'lucide-react';
 
 const Footer = () => {
     return (

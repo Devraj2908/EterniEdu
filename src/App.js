e90456670fd5e03f2d5b09c7a7ca88390c95d7d6
Devraj Gavit <dvrjgavit@gmail.com>
2026-01-28 00:43:13 +0530

    Remove react-helmet-async provider to fix build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
@@ -10,35 +9,40 @@ import SectionPage from './pages/Section/SectionPage';
 import Programming from './pages/Programming/Programming';
 import './App.css';
 
-// ... (imports)
-
 function App() {
   return (
-    <HelmetProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout><Home /></Layout>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
             <Layout>
               <Dashboard />
             </Layout>
-          } />
-          <Route path="/section/:id" element={
+          }
+        />
+        <Route
+          path="/section/:id"
+          element={
             <Layout>
               <SectionPage />
             </Layout>
-          } />
-          <Route path="/programming" element={
+          }
+        />
+        <Route
+          path="/programming"
+          element={
             <Layout>
               <Programming />
             </Layout>
-          } />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Router>
-    </HelmetProvider>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
